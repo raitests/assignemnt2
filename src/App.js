a6ea77c651bf7f440c2367a323214df27bb50fe0
Pratik Rai <rpratik6600@gmail.com>
2025-01-04 00:23:57 +0545

    all-complete

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="widget-container">
+      <div className="widget widget1">Widget 1</div>
+      <div className="widget widget2">Widget 2</div>
+      <div className="widget widget3 orange">Widget 3</div>
+      <div className="widget widget4">Widget 4</div>
+      <div className="widget widget5">Widget 5</div>
+      <div className="widget widget6 orange">Widget 6</div>
+    </div>
+  );
+}
+
+export default App;
